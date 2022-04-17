57be27664f4478d11995813b5e32e117dcba5d56
Milky01 <50903087+Milky01@users.noreply.github.com>
2022-04-17 16:24:30 +0800

    Update detail.js

--- detail.js
+++ detail.js
@@ -8,7 +8,7 @@ body = JSON.parse($response.body)
 body.data.askForLeaveDetail.applyStatus = 4
 body.data.askForLeaveDetail.beginTime = bt
 body.data.askForLeaveDetail.endTime = et
-body.data.askForLeaveDetail.reason = "我想出去吃汉堡王啊！"
+// body.data.askForLeaveDetail.reason = "我想出去吃汉堡王啊！"
 body.data.askForLeaveDetail.chains.pop()
 
 $done({ body: JSON.stringify(body) });
